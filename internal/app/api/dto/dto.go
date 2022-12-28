3634a0b08fa4dc6dbb3adb00d121b9d95ac33d14
kkodecaffeine <kkodecaffeine@gmail.com>
2022-12-29 07:59:02 +0900

    chore: add comments

--- internal/app/api/dto/dto.go
+++ internal/app/api/dto/dto.go
@@ -1,46 +1,46 @@
 package dto
 
 type PostSMSRequest struct {
-	Phone string `json:"phone" binding:"required,customPhone"`
+	Phone string `json:"phone" binding:"required,customPhone"` // 전화번호
 }
 
 type PostSMSResponse struct {
-	AuthNumber string `json:"authnumber"`
+	AuthNumber string `json:"authnumber"` // 인증번호
 }
 
 // 회원 가입
 type PostSignUpRequest struct {
-	AuthNumber string `json:"authnumber" binding:"required" validate:"len=6"`
-	Email      string `json:"email" binding:"required"`
-	NickName   string `json:"nickname" binding:"required"`
-	Name       string `json:"name" binding:"required"`
-	Password   string `json:"password" binding:"required"`
-	Phone      string `json:"phone" binding:"required,customPhone"`
+	AuthNumber string `json:"authnumber" binding:"required" validate:"len=6"` // 인증번호
+	Email      string `json:"email" binding:"required"`                       // 이메일
+	NickName   string `json:"nickname" binding:"required"`                    // 닉네임
+	Name       string `json:"name" binding:"required"`                        // 이름
+	Password   string `json:"password" binding:"required"`                    // 비밀번호
+	Phone      string `json:"phone" binding:"required,customPhone"`           // 전화번호
 }
 
 // 회원 로그인
 type PostSignInRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password" binding:"required"`
-	Phone    string `json:"phone" binding:"customPhone"`
+	Email    string `json:"email"`                       // 이메일
+	Password string `json:"password" binding:"required"` // 비밀번호
+	Phone    string `json:"phone" binding:"customPhone"` // 전화번호
 }
 
 // 회원 조회
 type GetUserResponse struct {
-	Id       string `json:"id"`
-	Email    string `json:"email"`
-	NickName string `json:"nickname"`
-	Name     string `json:"name"`
-	Phone    string `json:"phone"`
+	Id       string `json:"id"`       // 아이디
+	Email    string `json:"email"`    // 이메일
+	NickName string `json:"nickname"` // 닉네임
+	Name     string `json:"name"`     // 이름
+	Phone    string `json:"phone"`    // 전화번호
 }
 
 type GetUserWithTokenResponse struct {
-	AccessToken string `json:"accesstoken"`
-	Id          string `json:"id"`
-	Email       string `json:"email"`
-	NickName    string `json:"nickname"`
-	Name        string `json:"name"`
-	Phone       string `json:"phone"`
+	AccessToken string `json:"accesstoken"` // 토큰
+	Id          string `json:"id"`          // 아이디
+	Email       string `json:"email"`       // 이메일
+	NickName    string `json:"nickname"`    // 닉네임
+	Name        string `json:"name"`        // 이름
+	Phone       string `json:"phone"`       // 전화번호
 }
 
 type PostSignInResponse struct {
@@ -49,9 +49,9 @@ type PostSignInResponse struct {
 
 // 비밀번호 수정
 type PutPasswordRequest struct {
-	AuthNumber   string `json:"authnumber" binding:"required" validate:"len=6"`
-	Email        string `json:"email" binding:"required"`
-	Password     string `json:"password" binding:"required"`
-	NewPassword  string `json:"newpassword" binding:"required"`
-	Confirmation string `json:"confirmation" binding:"required"`
+	AuthNumber   string `json:"authnumber" binding:"required" validate:"len=6"` // 인증번호
+	Email        string `json:"email" binding:"required"`                       // 이메일
+	Password     string `json:"password" binding:"required"`                    // 기존 비밀번호
+	NewPassword  string `json:"newpassword" binding:"required"`                 // 신규 비밀번호
+	Confirmation string `json:"confirmation" binding:"required"`                // 신규 비밀번호 확인
 }
